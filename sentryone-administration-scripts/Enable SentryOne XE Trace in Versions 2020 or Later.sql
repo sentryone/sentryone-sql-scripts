11b49a1afe82ef6d2989e69b5ed8055affcf4b1d
Melissa Connors <40669268+MelissaConnors@users.noreply.github.com>
2020-12-21 08:40:34 -0500

    Create Enable SentryOne XE Trace in Versions 2020 or Later.sql

---
+++ sentryone-administration-scripts/Enable SentryOne XE Trace in Versions 2020 or Later.sql
@@ -0,0 +1,21 @@
+--https://docs.sentryone.com/help/enabling-extended-events
+
+--Disable old xe trace, if exists and enabled
+IF EXISTS
+(
+SELECT * FROM dbo.FeatureFlag
+WHERE [Name] = 'XeventsTrace' and [Enabled] = 1
+)
+UPDATE dbo.FeatureFlag
+SET [Enabled] = 0
+WHERE [Name] = 'XeventsTrace';
+--Enable RingBuffer XE trace
+IF NOT EXISTS
+(
+SELECT * FROM dbo.FeatureFlag
+WHERE [Name] = 'XEventsRingBuffer'
+)
+INSERT INTO dbo.FeatureFlag ([Name], [Enabled])
+VALUES ( 'XEventsRingBuffer', 1);
+
+SELECT * FROM dbo.FeatureFlag;
